ac9989ff4222ad5abed1c6fd6559726096b13a86
Nicholas Ng <ng.nicholas@outlook.com>
2015-07-12 22:39:46 +0800

    Scripted answers to quiz 1

--- Quiz-1 Scripting Alt.R
+++ Quiz-1 Scripting Alt.R
@@ -26,17 +26,21 @@ for (i in 1:length(dirDetails)) {
         answer1 <- dirDetails[[i]][4]
     }
 }
-print(paste0(">> The file is ", gsub("[:alpha:]", "", answer1), "Mb large."))
+print(paste0(">> The file is ", gsub("[:alpha:]", "", answer1),
+             "b large."))
 rm(dirDetails, answer1)
 
 print("# Question 2: The en_US.twitter.txt has how many lines of text?")
 answer2 <- strsplit(gsub("\\s{2,}", "", system("wc -l en_US.twitter.txt",
                                                intern = T)),
                     split = "\\s")[[1]][1]
-print(paste0(">> There are ", answer2, " lines of text in the file."))
+print(paste0(">> There are ",
+             format(as.numeric(answer2), big.mark = ",", trim = T),
+             " lines of text in the file."))
+rm(answer2)
 
-print("# Question 3: What is the length of the longest line seen in any of the
-      three en_US data sets?")
+print(paste0("# Question 3: What is the length of the longest line seen in ",
+             "any of the three en_US data sets?"))
 lineList <- strsplit(gsub("\\s{2,}", "", system("wc -L *.txt", intern = T)),
                      split = "\\s")
 lineList <- lineList[-length(lineList)]
@@ -44,35 +48,38 @@ lineFrame <- data.frame(matrix(unlist(lineList), nrow=length(lineList),
                                byrow=T), stringsAsFactors = F)
 lineFrame <- mutate(lineFrame, X1 = as.integer(X1))
 longLine <- max(lineFrame[1])
-print(paste0(">> The longest line is ", longLine, " characters long, and can be
-             found in the ", lineFrame[[2]][match(longLine, lineFrame[[1]])],
-             " file."))
+print(paste0(">> The longest line(s) is ",
+             format(longLine, big.mark = ",", trim = T),
+             " characters long, and can be found in the ",
+             lineFrame[[2]][match(longLine, lineFrame[[1]])], " file."))
+rm(lineList, lineFrame, longLine)
 
-print("# Question 4: In the en_US twitter data set, if you divide the number of
-      lines where the word 'love' (all lowercase) occurs by the number of lines
-      the word 'hate' (all lowercase) occurs, about what do you get?")
+print(paste0("# Question 4: In the en_US twitter data set, if you divide the ",
+             "number of lines where the word 'love' (all lowercase) occurs by ",
+             "the number of lines the word 'hate' (all lowercase) occurs, ",
+             "about what do you get?"))
+con <- file("en_US.twitter.txt", "r")
+twitLines <- readLines(con, encoding = "en_US")
+close(con)
+love <- sum(grepl("love", twitLines))
+hate <- sum(grepl("hate", twitLines))
+rship <- love / hate
+print(paste0(">> The love/hate relationship is ",
+             format(rship, big.mark = ",", trim = T, digits = 0),
+             "."))
+rm(con, love, hate, rship)
 
+print(paste0("# Question 5: The one tweet in the en_US twitter data set that ",
+             "matches the word 'biostats' says what?"))
+print(paste0(">> This is what it says: ",
+             grep("biostats", twitLines, ignore.case = T, value = T)))
 
-print("# Question 5: The one tweet in the en_US twitter data set that matches
-      the word 'biostats' says what?")
-
-print("# Question 6: How many tweets have the exact characters 'A computer once
-      beat me at chess, but it was no match for me at kickboxing'. (I.e. the
-      line matches those characters exactly.)")
-
-# Finding the longest line in the en_US data sets
-print("# Loading the blogs and news data and finding length of longest line...")
-dataBlogsNews <- lapply(corpText[1:2], as.character)
-maxBlogs <- max(nchar(dataBlogsNews$en_US.blogs.txt))
-maxNews <- max(nchar(dataBlogsNews$en_US.news.txt))
-rm(dataBlogsNews)
-print(paste("The longest line in en_US.blogs.txt is", maxBlogs,
-            "and the same for en_US.news.txt is", maxNews, "."))
-
-# Finding the ratio of lines with love/hate
-print("# Loading the twitter data and finding length of longest line...")
-dataTwit <- lapply(corpText[3], as.character)
-loveCount <- sum(grepl("love", dataTwit))
-hateCount <- sum(grepl("hate", dataTwit))
-loveHateRatio <- loveCount / hateCount
-rm(dataTwit)
+print(paste0("# Question 6: How many tweets have the exact characters 'A ",
+             "computer once beat me at chess, but it was no match for me at ",
+             "kickboxing'. (I.e. the line matches those characters exactly.)"))
+exactLines <- sum(grepl(paste0("A computer once beat me at chess, but it was ",
+                               "no match for me at kickboxing"), twitLines))
+print(paste0(">> There are a total of ",
+             format(exactLines, big.mark = ",", trim = T),
+             " such lines in the twitter data."))
+rm(list = ls())
