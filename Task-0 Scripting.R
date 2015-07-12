d0fd1677a34f9cef8d151443bfeb85567cf4265c
Nicholas Ng <ng.nicholas@outlook.com>
2015-07-12 17:53:34 +0800

    R script used to answer first quiz.

---
+++ Task-0 Scripting.R
@@ -0,0 +1,47 @@
+## Scripting file for Task 1
+
+# Loading packages for parallel computing and setting options
+library("cluster")
+library("parallel")
+library("doSNOW")
+coreNumber <- max(detectCores(),1)
+cluster <- makeCluster(coreNumber, type = "SOCK",outfile="")
+registerDoSNOW(cluster)
+
+# Loading package for NLP
+library("tm")
+
+# Defining directories and setting working directory
+print("# Defining and setting working directory...")
+dirWork <- "C:/Users/Nicholas/Documents/GitHub/Capstone/"
+dirData <- "./Data/"
+dirCorp <- "./Corpus/"
+if (dir.exists(dirCorp) == F) {
+    dir.create(dirCorp)
+}
+setwd(dirWork)
+
+# Creating a permanent corpus for working on files
+print("# Creating a permanent corpus to save memory...")
+corpText <- PCorpus(DirSource(dirData),
+                    readerControl = list(reader = readPlain,
+                                         language = "en_US"),
+                    dbControl = list(dbName = paste0(dirCorp, "ProjectTexts.db"),
+                                     dbType = "DB1"))
+
+# Finding the longest line in the en_US data sets
+print("# Loading the blogs and news data and finding length of longest line...")
+dataBlogsNews <- lapply(corpText[1:2], as.character)
+maxBlogs <- max(nchar(dataBlogsNews$en_US.blogs.txt))
+maxNews <- max(nchar(dataBlogsNews$en_US.news.txt))
+rm(dataBlogsNews)
+print(paste("The longest line in en_US.blogs.txt is", maxBlogs,
+            "and the same for en_US.news.txt is", maxNews, "."))
+
+# Finding the ratio of lines with love/hate
+print("# Loading the twitter data and finding length of longest line...")
+dataTwit <- lapply(corpText[3], as.character)
+loveCount <- sum(grepl("love", dataTwit))
+hateCount <- sum(grepl("hate", dataTwit))
+loveHateRatio <- loveCount / hateCount
+rm(dataTwit)
